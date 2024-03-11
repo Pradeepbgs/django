71d111727a6156c27f60bba54da4e8bbbb7e107d
pradeep kumar <kumarpradeepbgs@gmail.com>
2024-03-11 12:07:52 +0530

    now authentication is working

--- myapp/views.py
+++ myapp/views.py
@@ -13,26 +13,35 @@ def register(request):
         password = request.POST.get("password")
         existUser = User.objects.filter(username=username).exists()
         if existUser:
-            return JsonResponse({"msg":"user already exists with this username"})
-        hashed_password = make_password(password)
-        print(hashed_password)
+            return JsonResponse({"msg": "User already exists with this username"})
+        
         user = User.objects.create_user(
-            username=username.lower(),email=email,password=hashed_password)
-
+            username=username.lower(), email=email, password=password)
+        
         if user is not None:
-            return JsonResponse({"msg":"user created successfully"})
+            return JsonResponse({"msg": "User created successfully"})
+
         
 @csrf_exempt
 def user_login(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
+        
+        if not User.objects.filter(username=username).exists():
+            return JsonResponse({"msg": "Can't find user"})
 
-        user = authenticate(request, username=username, password=password)
+        user = authenticate(username=username, password=password)
         if user is not None:
-            login(request,user)
-            return JsonResponse({'user':"user logged in",'userData':user})
+            login(request, user)
+            user_data = {
+                'username': user.username,
+                'email': user.email,
+                'first_name': user.first_name,
+                'last_name': user.last_name
+            }
+            return JsonResponse({'user': "User logged in", 'userData': user_data})
         else:
-            return JsonResponse({"msg":"cant find user"})
+            return JsonResponse({"msg": "Invalid password"})
 
-    return JsonResponse({'msg':"cant find"})
+    return JsonResponse({'msg': "Method not allowed"})
