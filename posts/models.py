87fb7b696b996131696b1a713220539f43049598
pradeep kumar <kumarpradeepbgs@gmail.com>
2024-03-13 15:04:37 +0530

    remigrated

--- posts/models.py
+++ posts/models.py
@@ -8,21 +8,7 @@ class ImagePost(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
 
-class Comment(models.Model):
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, null=True, default=None)
-    post = models.ForeignKey(ImagePost,on_delete=models.CASCADE,null=True,default=None)
-    comment = models.CharField(max_length=200)
-    created_at = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return f'{self.user.username}: {self.comment[:20]}...'
-    
-
-class Like(models.Model):
-    liked_by = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    post = models.ForeignKey(ImagePost, on_delete=models.CASCADE, null=True)
-    comment = models.ForeignKey(Comment, on_delete=models.CASCADE, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
 
 
 
